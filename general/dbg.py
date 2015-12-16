2d7824a7c1cd36c1af945746c29fbb2dfcbc319d
Jaroslav Vondrejc <vondrejc@gmail.com>
2015-12-16 14:27:01 +0100

    clean up in general.dbg

--- general/dbg.py
+++ general/dbg.py
@@ -1,28 +1,5 @@
-import sys
 import time
 
-
-def p(t):
-    """ Print variable and type of it """
-    print('__')
-    print(t)
-    print(type(t))
-
-def pe(t):
-    """ Print variable, type of it, and finally exits the program """
-    print('__')
-    print(t)
-    print(type(t))
-    sys.exit()
-
-def start_time():
-    """ Starts both real and computational time """
-    t = [time.clock(), time.time()]
-    return t
-
-def get_time(t):
-    """ Measure and prints both real and computational time """
-    return [time.clock()-t[0], time.time()-t[1]]
 
 class Timer():
     def __init__(self, name='time', start=True):
