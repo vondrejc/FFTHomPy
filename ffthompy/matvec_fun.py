296cfb59abecb48e39c7e466a369db1d2ca318af
Jaroslav Vondrejc <vondrejc@gmail.com>
2017-02-16 17:57:21 +0100

    Grid.product()

--- ffthompy/matvec_fun.py
+++ ffthompy/matvec_fun.py
@@ -31,6 +31,15 @@ class Grid():
         return Grid.get_xil(N, Y)
 
     @staticmethod
+    def get_product(xi):
+        xis = np.atleast_2d(xi[0])
+        for ii in range(1, len(xi)):
+            xis_new = np.tile(xi[ii], xis.shape[1])
+            xis_old = np.repeat(xis, xi[ii].size, axis=1)
+            xis = np.vstack([xis_old, xis_new])
+        return xis
+
+    @staticmethod
     def get_coordinates(N, Y):
         """
         It produces coordinates of the set of nodal points
