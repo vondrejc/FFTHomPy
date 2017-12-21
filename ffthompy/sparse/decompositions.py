d16d81c07ea03555b641614103a70718746a6a40
Jaroslav Vondrejc <vondrejc@gmail.com>
2017-12-21 11:33:29 +0100

    clean up

--- ffthompy/sparse/decompositions.py
+++ ffthompy/sparse/decompositions.py
@@ -1,55 +1,55 @@
 import numpy as np
 
 
-def dCA_matrix_input(M,   k, tol=1e-16): 
+def dCA_matrix_input(M, k, tol=1e-16):
     """
     diagonally pivoted Cross Approximation, a low rank approximation algorithm
-    only work with symmetric positive semi-definite (SPSD) matrices, e.g. covariance matrices or correlation matrices  
-    
+    only work with symmetric positive semi-definite (SPSD) matrices, e.g. covariance matrices or correlation matrices
+
     it approximates a N-by-N SPSD matrix M with a N-by-k matrix A , so that M and A*A' is roughly equal, it also gives the maximum error in the output.
-    
+
     input:
             M-- a SPSD matrix
 
             k -- integer, size of approximation, k <= N
             tol -- float, error tolerance in maximum norm
-            
+
     output:
 
             A -- N-by-k matrix
- 
+
             max_err -- maximum element-wise error of this approximation
-            
-            k_actual -- actual k based on tol 
-            
-    """ 
-    N = M.shape[0] 
-      
-    diagonal = np.copy(np.diag(M))
-   
-    A=np.empty((N, k),dtype=np.float) 
-    
-    for i in range(0,k):
+
+            k_actual -- actual k based on tol
+
+    """
+    N=M.shape[0]
+
+    diagonal=np.copy(np.diag(M))
+
+    A=np.empty((N, k), dtype=np.float)
+
+    for i in range(0, k):
 
         p=diagonal.argmax()
- 
-        dia_max = diagonal[p]
- 
-        A[:, i] =   (M[:,p] - np.dot(A[:, :i], A[p, :i].T))/ np.sqrt(dia_max)   
-        
-        diagonal = diagonal - A[:, i]**2 
-    
-        max_err= np.max( diagonal ) 
-        #print max_err
-        if max_err <= tol:
+
+        dia_max=diagonal[p]
+
+        A[:, i]=(M[:, p]-np.dot(A[:, :i], A[p, :i].T))/np.sqrt(dia_max)
+
+        diagonal=diagonal-A[:, i]**2
+
+        max_err=np.max(diagonal)
+        # print max_err
+        if max_err<=tol:
             break
-    
-    k_actual = i+1
-    if k != k_actual:
-        print "The actual rank k is %d in stead of %d" & (k_actual, k)
-        
-    A = A[:, :k_actual]
-    return A, k_actual, max_err 
+
+    k_actual=i+1
+    if k!=k_actual:
+        print "The actual rank k is %d in stead of %d"&(k_actual, k)
+
+    A=A[:, :k_actual]
+    return A, k_actual, max_err
 
 def my_epsilon(x):
 
@@ -148,75 +148,75 @@ def PCA_matrix_input(C, N, M, k, tol=2.22e-16):
     r"""
     Partially pivoted Cross Approximation. A low rank approximation algorithm that approximates an N-by-N SPSD matrix :math:`C` with an N-by-k matrix :math:`A` and a k-by-M matrix :math:`B`  so that :math:`C` and :math:`A\;B` is rou
     ghly equal. It also gives an error estimate in the output.
-            
-    The method returns matrices *A* and *B*, together with an Frobenius error estimate *err* 
-              
-    :param C: A matrix 
-    :type C: float   
+
+    The method returns matrices *A* and *B*, together with an Frobenius error estimate *err*
+
+    :param C: A matrix
+    :type C: float
     :param N: Number of rows of C
     :type N: integer
     :param M: Number of columns of C
     :type M: integer
     :param k: Rank of approximation, k <= N
-    :type k: integer        
+    :type k: integer
     :param tol: Frobenius error tolerance
     :type tol: float
-      
+
     :rtype: tuple (np.ndarray (2D), np.ndarray (2D) , int, float)
     """
-    
-    A = np.zeros((N, k))
-    B = np.zeros((k, M))
-    Pi = np.array([])
-    Pj = np.array([])
-    		
-    istar = 0
-    i_list = np.arange(0, N, 1)
-    j_list = np.arange(0, M, 1)  
-    		
+
+    A=np.zeros((N, k))
+    B=np.zeros((k, M))
+    Pi=np.array([])
+    Pj=np.array([])
+
+    istar=0
+    i_list=np.arange(0, N, 1)
+    j_list=np.arange(0, M, 1)
+
     for i in range(0, k):
-        j_list_small = np.setdiff1d(j_list, Pj)
-        Row = C[istar,:]
-        jstar = np.argmax(abs(Row[j_list_small]))
-        jstar = j_list_small[jstar]
-    			
-        Pj = np.hstack((Pj, jstar)) 
-    			
-        if i > 0:
-            max_value = Row[jstar] - np.dot(A[istar, 0:i], B[0:i, jstar])
-    			
+        j_list_small=np.setdiff1d(j_list, Pj)
+        Row=C[istar, :]
+        jstar=np.argmax(abs(Row[j_list_small]))
+        jstar=j_list_small[jstar]
+
+        Pj=np.hstack((Pj, jstar))
+
+        if i>0:
+            max_value=Row[jstar]-np.dot(A[istar, 0:i], B[0:i, jstar])
+
         else:
-            max_value = Row[jstar]
-    			
-        if max_value == 0:
+            max_value=Row[jstar]
+
+        if max_value==0:
             break
-        Column = C[:,jstar]
-        A[:, i] = Column - np.dot(A[:, 0:i], B[0:i, jstar])
-        B[i, :] = (Row - np.dot(A[istar, 0:i], B[0:i, :])) / max_value
-    			
-        Pi = np.hstack((Pi, istar))             
-        i_list_small = np.setdiff1d(i_list, Pi)
-    			
-        if i_list_small.shape[0] < 1:
+        Column=C[:, jstar]
+        A[:, i]=Column-np.dot(A[:, 0:i], B[0:i, jstar])
+        B[i, :]=(Row-np.dot(A[istar, 0:i], B[0:i, :]))/max_value
+
+        Pi=np.hstack((Pi, istar))
+        i_list_small=np.setdiff1d(i_list, Pi)
+
+        if i_list_small.shape[0]<1:
             break
-        istar = np.argmax(abs(Column[i_list_small]))
-        istar = i_list_small[istar]
-    		
-        err = np.linalg.norm(A[:, i]) * np.linalg.norm(B[i, :])  
-        #print err
-        if err<= tol:
+        istar=np.argmax(abs(Column[i_list_small]))
+        istar=i_list_small[istar]
+
+        err=np.linalg.norm(A[:, i])*np.linalg.norm(B[i, :])
+        # print err
+        if err<=tol:
             break
-    
+
     k_actual=i+1
-    
-    if np.sum(np.abs(A[:,i]))<=2.22e-16 or np.sum(np.abs(B[i,:]))<=2.22e-16 :
-        k_actual = k_actual-1 
-    
-    if k != k_actual:
-        print "Warning: the output's actual rank k is %d in stead of %d" %(k_actual, k)
-    A = A[:,:k_actual]
-    B = B[:k_actual, :]
-    
+
+    if np.sum(np.abs(A[:, i]))<=2.22e-16 or np.sum(np.abs(B[i, :]))<=2.22e-16 :
+        k_actual=k_actual-1
+
+    if k!=k_actual:
+        print "Warning: the output's actual rank k is %d in stead of %d"%(k_actual, k)
+    A=A[:, :k_actual]
+    B=B[:k_actual, :]
+
     return A, B, k_actual, err
 
 
