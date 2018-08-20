0bb288821e78ab5a7bb36b64c6d5312dabcee8bb
Jaroslav Vondrejc <vondrejc@gmail.com>
2018-08-20 16:30:10 +0200

    removing redundant

--- diffusion_sparse_Ga_v3_use_tucker.py
+++ diffusion_sparse_Ga_v3_use_tucker.py
@@ -13,52 +13,44 @@ from ffthompy.sparse.homogenisation_tucker import homog_sparse
 
 # PARAMETERS ##############################################################
 dim=2
-N=dim*(25,)
+N=25
 pars=Struct(dim=dim, # number of dimensions (works for 2D and 3D)
-            N=N, # number of voxels (assumed equal for all directions)
+            N=dim*(N,), # number of voxels (assumed equal for all directions)
             Y=np.ones(dim),
             Amax=10., # material contrast
-            maxiter=20,
+            maxiter=10,
             tol=None,
             rank=10,
             solver={'tol':1e-4}
             )
 
 pars_sparse=pars.copy()
-pars_sparse.update(Struct())
+pars_sparse.update(Struct(N=dim*(3*N,),))
 
-calc_eigs=1
-debug=False
-
-# auxiliary values
-Nbar=2*np.array(N)-1
-prodN=np.prod(np.array(N)) # number of grid points
-ndof=dim*prodN # number of degrees-of-freedom
-vec_shape=(dim,)+N # shape of the vector for storing DOFs
+# auxiliary operator
+Nbar = lambda N: 2*np.array(N)-1
 
 # PROBLEM DEFINITION ######################################################
-E=np.zeros(vec_shape); E[0]=1. # set macroscopic loading
-
-# sparse A
 mat_conf={'inclusions': ['square', 'otherwise'],
           'positions': [0.*np.ones(dim), ''],
           'params': [0.6*np.ones(dim), ''], # size of sides
           'vals': [10*np.eye(dim), 1.*np.eye(dim)],
           'Y': np.ones(dim),
-          'P': N,
+          'P': pars.N,
           'order': 0, }
 
 mat=Material(mat_conf)
 mats=SparseMaterial(mat_conf)
 
-Agani=matrix2tensor(mat.get_A_GaNi(N, primaldual='primal'))
-Aganis=mats.get_A_GaNi(N, primaldual='primal', k=2)
+# Agani=matrix2tensor(mat.get_A_GaNi(N, primaldual='primal'))
+# Aganis=mats.get_A_GaNi(N, primaldual='primal', k=2)
+# print(np.linalg.norm(Agani.val[0, 0]-Aganis.full()))
 
-Aga=matrix2tensor(mat.get_A_Ga(Nbar, primaldual='primal'))
-Agas=mats.get_A_Ga(Nbar, primaldual='primal', order=0, k=2)
+Aga=matrix2tensor(mat.get_A_Ga(Nbar(pars.N), primaldual='primal'))
+Agas=mats.get_A_Ga(Nbar(pars_sparse.N), primaldual='primal', order=0, k=2)
 
-print(np.linalg.norm(Agani.val[0, 0]-Aganis.full()))
-print(np.linalg.norm(Aga.val[0, 0]-Agas.full()))
+if np.array_equal(pars.N, pars_sparse.N):
+    print(np.linalg.norm(Aga.val[0, 0]-Agas.full()))
 
 # OPERATORS ###############################################################
 print('\n== Full solution with potential by CG ===========')
@@ -71,7 +63,8 @@ print('homogenised properties (component 11) = {}'.format(resS.AH))
 
 print(resS.Fu)
 print('iterations={}'.format(resS.solver['kit']))
-print('norm(dif)={}'.format(np.linalg.norm(resP.Fu.val-resS.Fu.full())))
+if np.array_equal(pars.N, pars_sparse.N):
+    print('norm(dif)={}'.format(np.linalg.norm(resP.Fu.val-resS.Fu.full())))
 print('norm(resP)={}'.format(resS.solver['norm_res']))
 print('memory efficiency = {0}/{1} = {2}'.format(resS.Fu.size, resP.Fu.val.size, resS.Fu.size/resP.Fu.val.size))
 
