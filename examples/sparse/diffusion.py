3b75fa8b00058cfed46dab329b3fffa92b12a909
Jaroslav Vondrejc <vondrejc@gmail.com>
2019-07-05 13:58:03 +0200

    clean up

--- examples/sparse/diffusion.py
+++ examples/sparse/diffusion.py
@@ -2,22 +2,19 @@ from __future__ import division, print_function
 
 import numpy as np
 
-from ffthompy import Timer, Struct
+from ffthompy import Struct
 from ffthompy.materials import Material
 from ffthompy.sparse.homogenisation import (homog_Ga_full_potential, homog_GaNi_full_potential,
                                             homog_Ga_sparse, homog_GaNi_sparse)
 from ffthompy.sparse.materials import SparseMaterial
 from examples.sparse.material_setting import getMat_conf,recover_Aga,recover_Agani
 
-import os
-import sys
-
 
 # PARAMETERS ##############################################################
-dim=2
+dim=3
 N=5*3**1
 material=0
-kind=0
+kind=1
 kind_list=['cano','tucker','tt']
 
 pars=Struct(dim=dim, # number of dimensions (works for 2D and 3D)
@@ -32,8 +29,8 @@ pars_sparse.update(Struct(kind=kind_list[kind], # type of sparse tensor: 'cano',
                           rank=15, # rank of solution vector
                           precond_rank=10,
                           tol=None,
-                          N=dim*(1*N,),
-                          solver=dict(method='mr', #  method could be 'Richardson'(r),'minimal_residual'(mr), or 'Chebyshev'(c)
+                          N=dim*(N,),
+                          solver=dict(method='mr', # method could be 'Richardson'(r),'minimal_residual'(mr), or 'Chebyshev'(c)
                                       approx_omega=False, # inner product of tuckers could be so slow
                                                           # that using an approximate omega could gain.
                                       eigrange=[0.6,50], # for Chebyshev solver
@@ -45,7 +42,7 @@ pars_sparse.update(Struct(kind=kind_list[kind], # type of sparse tensor: 'cano',
 print('== format={}, N={}, dim={}, material={} ===='.format(pars_sparse.kind,
                                                             N, dim, material))
 
-### get material settings for experiment
+# get material settings for experiment
 pars, pars_sparse, mat_conf = getMat_conf(material, pars, pars_sparse)
 
 # generating material coefficients
@@ -64,7 +61,6 @@ Agani.val=recover_Agani(Agani,Aganis)
 
 pars_sparse.update(Struct(alpha=0.5*(Agani[0, 0].min()+Agani[0, 0].max())))
 
-#######OPERATORS ###############################################################
 print('\n== Full solution with potential by CG (GaNi)===========')
 resP_GaNi=homog_GaNi_full_potential(Agani, Aga, pars)
 print('mean of solution={}'.format(resP_GaNi.Fu.mean()))
@@ -81,11 +77,7 @@ print('mean of solution={}'.format(resS_Ga.Fu.mean()))
 print('homogenised properties (component 11) = {}'.format(resS_Ga.AH))
 print(resS_Ga.Fu)
 print('iterations={}'.format(resS_Ga.solver['kit']))
-#if np.array_equal(pars.N, pars_sparse.N):
-#    print('norm(dif)={}'.format(np.linalg.norm(resP_Ga.Fu.fourier().val-resS_Ga.Fu.fourier().full().val)))
 print('norm(resP)={}'.format(resS_Ga.solver['norm_res']))
-
-#print('memory efficiency = {0}/{1} = {2}'.format(resS_Ga.Fu.memory, resP_Ga.Fu.val.size, resS_Ga.Fu.memory/resP_Ga.Fu.val.size))
 
 print('\n== SPARSE  solver with preconditioner (GaNi) =======================')
 resS_GaNi=homog_GaNi_sparse(Aganis, Agas, pars_sparse)
@@ -97,6 +89,5 @@ print('iterations={}'.format(resS_GaNi.solver['kit']))
 if np.array_equal(pars.N, pars_sparse.N):
     print('norm(dif)={}'.format(np.linalg.norm(resP_GaNi.Fu.fourier().val-resS_GaNi.Fu.fourier().full().val)))
 print('norm(resP)={}'.format(resS_GaNi.solver['norm_res']))
-#print('memory efficiency = {0}/{1} = {2}'.format(resS_GaNi.Fu.memory, resP_GaNi.Fu.val.size, resS_GaNi.Fu.memory/resP_GaNi.Fu.val.size))
 
 print('END')
