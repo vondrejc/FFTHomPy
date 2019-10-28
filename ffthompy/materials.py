fix of computation of eigenvalues in Tensor

ffthompy/materials.py
@@ -185,18 +185,28 @@ class Material():
         A : numpy.array
             material coefficients at coordinates (coord)
         """
+        N=coord.shape[1:]
+
         if 'fun' in self.conf:
             fun = self.conf['fun']
             A_val = fun(coord)
+            shp=A_val.shape[:A_val.ndim-len(N)]
         else:
-            A_val = np.zeros(self.conf['vals'][0].shape + coord.shape[1:])
+            shp=self.conf['vals'][0].shape
+            A_val = np.zeros(shp+N)
             topos = self.get_topologies(coord)
 
+            astr='abcdefgh'
+            istr='ijklmnopqrst'
+
             for ii in np.arange(len(self.conf['inclusions'])):
-                A_val += np.einsum('ij...,k...->ijk...', self.conf['vals'][ii], topos[ii])
+                stroper='{0},{1}->{0}{1}'.format(astr[:len(shp)], istr[:len(N)])
+                A_val += np.einsum(stroper, self.conf['vals'][ii], topos[ii])
         if tensor:
-            return Tensor(name='A_GaNi', val=A_val, order=2, N=coord[0].shape,
-                          Y=self.Y, multype=21, Fourier=False, origin='c')
+            multypes={'2':21, '4': 42}
+            return Tensor(name='A_GaNi', val=A_val, order=len(shp), N=coord[0].shape,
+                          Y=self.Y, multype=multypes['{}'.format(len(shp))],
+                          Fourier=False, origin='c')
         else:
             return Matrix(name='A_GaNi', val=A_val, Fourier=False)
 

ffthompy/tensors/objects.py
@@ -363,19 +363,19 @@ class Tensor(TensorFuns):
             eigfun=np.linalg.eigvals
 
         if self.order==2:
-            eigs=np.zeros(self.shape[-1]+self.N)
+            eigs=np.zeros(self.N+(self.shape[0],))
             for ind in np.ndindex(self.N):
-                mat=self.val[:, :][ind]
-                eigs.append(eigfun(mat))
+                mat=self.val[(slice(None), slice(None))+ind]
+                eigs[ind]=eigfun(mat)
+
         elif self.order==4:
             if mandel:
                 matrixfun=lambda x: ElasticTensor.create_mandel(x)
                 d=self.shape[2]
                 eigdim=d*(d+1)/2
             else:
-                matshape=(self.shape[0]*self.shape[1], self.shape[2]*self.shape[3])
-                matrixfun=lambda x: np.reshape(val[ind], matshape)
                 eigdim=self.shape[2]*self.shape[3]
+                matrixfun=lambda x: np.reshape(x, 2*(eigdim,))
 
             eigs=np.zeros(self.N+(eigdim,))
             val=np.copy(self.val)
@@ -383,10 +383,9 @@ class Tensor(TensorFuns):
                 val=np.rollaxis(val, self.val.ndim-self.dim+ii, ii)
 
             for ind in np.ndindex(*self.N):
-                mat=matrixfun(val[ind])
-                eigs[ind]=eigfun(mat)
+                eigs[ind]=eigfun(matrixfun(val[ind]))
 
-        eigs=np.array(eigs)
+        eigs=np.rollaxis(np.array(eigs), -1, 0)
         if sort:
             eigs=np.sort(eigs, axis=0)
         return eigs

ffthompy/unittest_materials.py
@@ -1,9 +1,8 @@
 import unittest
 import numpy as np
-from numpy.linalg import norm
 from ffthompy import PrintControl
 from ffthompy.materials import Material
-from ffthompy.tensors.operators import matrix2tensor
+from ffthompy.mechanics.matcoef import ElasticTensor
 
 prt=PrintControl()
 
@@ -58,6 +57,50 @@ class Test_materials(unittest.TestCase):
                 self.assertAlmostEqual(0, np.linalg.norm(Aga0.val[0, 0]-Aga1.val[0, 0]),
                                        msg=msg, delta=1e-13)
         print('...ok')
+
+    def test_eigenvalues(self):
+        print('\nEigenvalues of material tensors...')
+
+        # SCALAR PROBLEM
+        dim=2
+        scalar={'inclusions': ['square', 'otherwise'],
+                'positions': [np.zeros(dim), ''],
+                'params': [0.6*np.ones(dim), ''],
+                'vals': [10*np.eye(dim), 1.*np.eye(dim)],
+                'N': dim*(5,),
+                'Y': np.ones(dim),
+                'order': None,}
+
+        # ELASTICITY
+        dim = 3
+        matcoefM=ElasticTensor(bulk=1, mu=1)
+        matcoefI=ElasticTensor(bulk=10, mu=5)
+
+        elastic={'inclusions': ['square', 'otherwise'],
+                 'positions': [np.zeros(dim), ''],
+                 'params': [0.6*np.ones(dim), ''], # size of sides
+                 'vals': [matcoefI.mandel, matcoefM.mandel],
+                 'Y': np.ones(dim),
+                 'order': None,
+                 'N': dim*(5,),
+                 'P': dim*(5,), }
+
+        elastic2={'inclusions': ['square', 'otherwise'],
+                 'positions': [np.zeros(dim), ''],
+                 'params': [0.6*np.ones(dim), ''], # size of sides
+                 'vals': [matcoefI.val, matcoefM.val],
+                 'Y': np.ones(dim),
+                 'order': None,
+                 'N': dim*(5,),
+                 'P': dim*(5,), }
+
+        for mat_conf in [scalar, elastic, elastic2]:
+            mat=Material(mat_conf)
+            A=mat.get_A_GaNi(mat_conf['N'], primaldual='primal')
+            _=A.calc_eigs(sort=True, symmetric=True)
+
+        print('...ok')
+
 
 if __name__ == "__main__":
     unittest.main()